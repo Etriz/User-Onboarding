88c9d97991adf0b832a0094b541e9ee98141dcd9
ryanp <ryanpanimation@gmail.com>
2020-04-29 23:52:02 -0500

    test for non-duplicate email

--- app/cypress/integration/form_test.js
+++ app/cypress/integration/form_test.js
@@ -1,8 +1,16 @@
 describe("test form", () => {
-  it("test inputs", function () {
+  it("test inputs", () => {
     cy.visit("http://localhost:3000");
     cy.get("#name").type("test").should("have.value", "test");
     cy.get("#email").type("email@email.com").should("have.value", "email@email.com");
+    cy.get("#password").type("password").should("have.value", "password");
+    cy.get("#terms").check().should("be.checked");
+    cy.get("button").click();
+  });
+  it("test for non-duplicate email", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("#name").type("test").should("have.value", "test");
+    cy.get("#email").type("waffle@syrup.com").should("have.value", "waffle@syrup.com");
     cy.get("#password").type("password").should("have.value", "password");
     cy.get("#terms").check().should("be.checked");
     cy.get("button").click();
